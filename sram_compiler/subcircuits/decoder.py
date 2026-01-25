9bc567a354087d60322ad1fce78f2cccd15a8312
MaJunhao-6 <marvin660@163.com>
2026-01-25 16:45:42 +0800

    Update decoder.py

--- sram_compiler/subcircuits/decoder.py
+++ sram_compiler/subcircuits/decoder.py
@@ -1,131 +1,7 @@
-from PySpice.Spice.Netlist import SubCircuitFactory, Circuit
-from PySpice.Unit import u_Ohm, u_pF, u_V, u_ns
+from PySpice.Unit import u_Ohm, u_pF
 from .base_subcircuit import BaseSubcircuit
-from numpy.f2py.crackfortran import endifs
-import math
 from math import ceil, log2
-from .standard_cell import PNAND3_for_decoder,Pinv_for_decoder,PNAND2_for_decoder # type: ignore
-
-class AND3(BaseSubcircuit):  # ////////三输入与门=与非门+非门
-
-    NAME = "AND3"
-    NODES = ('VDD', 'VSS', 'A', 'B', 'C', 'Z')
-
-    def __init__(self, nmos_model_name, pmos_model_name,
-                 # Base widths for NAND gate transistors
-                 base_nand_pmos_width=0.27e-6, base_nand_nmos_width=0.18e-6,
-                 # Base widths for Inverter transistors
-                 base_inv_pmos_width=0.27e-6, base_inv_nmos_width=0.09e-6,
-                 length=0.05e-6,
-                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF,sweep_decoder=False,
-                 pmos_model_choices = 'PMOS_VTG',nmos_model_choices = 'NMOS_VTG'
-                 ):
-
-        super().__init__(
-            nmos_model_name, pmos_model_name,
-            base_nand_nmos_width, base_nand_pmos_width, length,
-            w_rc=w_rc, pi_res=pi_res, pi_cap=pi_cap,
-        )
-        self.sweep_decoder=sweep_decoder
-        self.pmos_model_choices=pmos_model_choices
-        self.nmos_model_choices=nmos_model_choices
-        # This is the nand gate
-        self.nand_gate = PNAND3_for_decoder(nmos_model_name=nmos_model_name,
-                                pmos_model_name=pmos_model_name,
-                                base_pmos_width=base_nand_pmos_width,
-                                base_nmos_width=base_nand_nmos_width,
-                                length=length,sweep_decoder=self.sweep_decoder,
-                                pmos_model_choices = self.pmos_model_choices,
-                                nmos_model_choices = self.nmos_model_choices
-                                )
-        self.subcircuit(self.nand_gate)  # 添加与非门电路
-
-        # This is the inverter for driving WLs
-        self.inv_driver = Pinv_for_decoder(nmos_model_name=nmos_model_name,
-                                        pmos_model_name=pmos_model_name,
-                                        base_pmos_width=base_inv_pmos_width,
-                                        base_nmos_width=base_inv_nmos_width,
-                                        length=length,sweep_decoder=self.sweep_decoder,
-                                        pmos_model_choices = self.pmos_model_choices,
-                                        nmos_model_choices = self.nmos_model_choices
-                                        )
-        self.subcircuit(self.inv_driver)  # 添加反相器电路
-
-        self.add_and3_components()
-
-    def add_and3_components(self):
-        
-        a_node = 'A'
-        b_node = "B"
-        c_node = "C"
-        zb_node = "zb_int"
-        z_node = "Z"
-
-        """ Instantiate the `PNAND3` and `Pinv` gates """  # 实例化
-        self.X(f'PNAND3', self.nand_gate.name,
-               'VDD', 'VSS', a_node, b_node, c_node, zb_node)
-        self.X(f'PINV', self.inv_driver.name,
-               'VDD', 'VSS', zb_node, z_node)
-
-class AND2(BaseSubcircuit):  # ////////二输入与门=二输入与非门+非门
-
-    NAME = "AND2"
-    NODES = ('VDD', 'VSS', 'A', 'B', 'Z')
-
-    def __init__(self, nmos_model_name, pmos_model_name,
-                 # Base widths for NAND gate transistors
-                 base_nand_pmos_width=0.27e-6, base_nand_nmos_width=0.18e-6,
-                 # Base widths for Inverter transistors
-                 base_inv_pmos_width=0.27e-6, base_inv_nmos_width=0.09e-6,
-                 length=0.05e-6,
-                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF,sweep_decoder=False,
-                 pmos_model_choices = 'PMOS_VTG',nmos_model_choices = 'NMOS_VTG'
-                 ):
-
-        super().__init__(
-            nmos_model_name, pmos_model_name,
-            base_nand_nmos_width, base_nand_pmos_width, length,
-            w_rc=w_rc, pi_res=pi_res, pi_cap=pi_cap,
-        )
-        self.sweep_decoder=sweep_decoder
-        self.pmos_model_choices=pmos_model_choices
-        self.nmos_model_choices=nmos_model_choices
-        # This is the nand gate
-        self.nand_gate = PNAND2_for_decoder(nmos_model_name=nmos_model_name,
-                                pmos_model_name=pmos_model_name,
-                                base_pmos_width=base_nand_pmos_width,
-                                base_nmos_width=base_nand_nmos_width,
-                                length=length,sweep_decoder=self.sweep_decoder,
-                                pmos_model_choices = self.pmos_model_choices,
-                                nmos_model_choices = self.nmos_model_choices
-                                )
-        self.subcircuit(self.nand_gate)  # 添加与非门电路
-
-        # This is the inverter for driving WLs
-        self.inv_driver = Pinv_for_decoder(nmos_model_name=nmos_model_name,
-                                        pmos_model_name=pmos_model_name,
-                                        base_pmos_width=base_inv_pmos_width,
-                                        base_nmos_width=base_inv_nmos_width,
-                                        length=length,sweep_decoder=self.sweep_decoder,
-                                        pmos_model_choices = self.pmos_model_choices,
-                                        nmos_model_choices = self.nmos_model_choices
-                                        )
-        self.subcircuit(self.inv_driver)  # 添加反相器电路
-
-        self.add_and3_components()
-
-    def add_and3_components(self):
-       
-        a_node = 'A'
-        b_node = "B"
-        zb_node = "zb_int"
-        z_node = "Z"
-
-        """ Instantiate the `PNAND3` and `Pinv` gates """  # 实例化
-        self.X(f'PNAND3', self.nand_gate.name,
-               'VDD', 'VSS', a_node, b_node, zb_node)
-        self.X(f'PINV', self.inv_driver.name,
-               'VDD', 'VSS', zb_node, z_node)
+from .standard_cell import Pinv,AND3,AND2 # type: ignore
 
 
 class DECODER3_8(BaseSubcircuit):  # 38译码器+使能EN端
@@ -133,78 +9,64 @@ class DECODER3_8(BaseSubcircuit):  # 38译码器+使能EN端
     NAME = "DECODER3_8"
     NODES = ('VDD', 'VSS','EN', 'A0', 'A1', 'A2', 'WL0', 'WL1', 'WL2', 'WL3', 'WL4', 'WL5', 'WL6', 'WL7')
 
-    def __init__(self, nmos_model_name, pmos_model_name,
+    def __init__(self, nmos_model_inv, pmos_model_inv, nmos_model_nand, pmos_model_nand,
                  # Base widths for NAND gate transistors
-                 base_nand_pmos_width=0.27e-6, base_nand_nmos_width=0.18e-6,
+                 nand_pmos_width=0.27e-6, nand_nmos_width=0.18e-6,
                  # Base widths for Inverter transistors
-                 base_inv_pmos_width=0.27e-6, base_inv_nmos_width=0.09e-6,
+                 inv_pmos_width=0.27e-6, inv_nmos_width=0.09e-6,
                  length=0.05e-6,
-                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF,sweep_decoder=False,
-                 pmos_model_choices = 'PMOS_VTG',nmos_model_choices = 'NMOS_VTG'
+                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF
                  ):
 
         super().__init__(
-            nmos_model_name, pmos_model_name,
-            base_nand_nmos_width, base_nand_pmos_width, length,
+            nmos_model_inv, pmos_model_inv,
+            nand_nmos_width, nand_pmos_width, length,
             w_rc=w_rc, pi_res=pi_res, pi_cap=pi_cap,
         )
-        self.sweep_decoder=sweep_decoder
-        self.pmos_model_choices=pmos_model_choices
-        self.nmos_model_choices=nmos_model_choices
+
         # 创建三个非门用于生成输入的反相
-        self.inv_A0 = Pinv_for_decoder(
-            nmos_model_name=nmos_model_name,
-            pmos_model_name=pmos_model_name,
-            base_pmos_width=base_inv_pmos_width,
-            base_nmos_width=base_inv_nmos_width,
+        self.inv_A0 = Pinv(
+            nmos_model=nmos_model_inv,
+            pmos_model=pmos_model_inv,
+            pmos_width=inv_pmos_width,
+            nmos_width=inv_nmos_width,
             length=length,
-            sweep_decoder=self.sweep_decoder,
-            pmos_model_choices = self.pmos_model_choices,
-            nmos_model_choices = self.nmos_model_choices
         )
         self.subcircuit(self.inv_A0)
 
-        self.inv_A1 = Pinv_for_decoder(
-            nmos_model_name=nmos_model_name,
-            pmos_model_name=pmos_model_name,
-            base_pmos_width=base_inv_pmos_width,
-            base_nmos_width=base_inv_nmos_width,
+        self.inv_A1 = Pinv(
+            nmos_model=nmos_model_inv,
+            pmos_model=pmos_model_inv,
+            pmos_width=inv_pmos_width,
+            nmos_width=inv_nmos_width,
             length=length,
-            sweep_decoder=self.sweep_decoder,
-            pmos_model_choices = self.pmos_model_choices,
-            nmos_model_choices = self.nmos_model_choices
         )
         self.subcircuit(self.inv_A1)
 
-        self.inv_A2 =Pinv_for_decoder(
-            nmos_model_name=nmos_model_name,
-            pmos_model_name=pmos_model_name,
-            base_pmos_width=base_inv_pmos_width,
-            base_nmos_width=base_inv_nmos_width,
+        self.inv_A2 = Pinv(
+            nmos_model=nmos_model_inv,
+            pmos_model=pmos_model_inv,
+            pmos_width=inv_pmos_width,
+            nmos_width=inv_nmos_width,
             length=length,
-            sweep_decoder=self.sweep_decoder,
-            pmos_model_choices = self.pmos_model_choices,
-            nmos_model_choices = self.nmos_model_choices
         )
-        self.subcircuit(self.inv_A2)
+        self.subcircuit(self.inv_A2)    
+
 
         # 创建八个三输入与门
         self.and_gates = []
         for _ in range(8):
             and_gate = AND3(
-                nmos_model_name=nmos_model_name,
-                pmos_model_name=pmos_model_name,
-                base_nand_pmos_width=base_nand_pmos_width,
-                base_nand_nmos_width=base_nand_nmos_width,
-                base_inv_pmos_width=base_inv_pmos_width,
-                base_inv_nmos_width=base_inv_nmos_width,
+                nmos_model_nand=nmos_model_nand,
+                pmos_model_nand=pmos_model_nand,
+                nmos_model_inv=nmos_model_inv,
+                pmos_model_inv=pmos_model_inv,
+                nand_pmos_width=nand_pmos_width,
+                nand_nmos_width=nand_nmos_width,
+                inv_pmos_width=inv_pmos_width,
+                inv_nmos_width=inv_nmos_width,
                 length=length,
                 w_rc=w_rc,
-                pi_res=pi_res,
-                pi_cap=pi_cap,
-                sweep_decoder=self.sweep_decoder,
-                pmos_model_choices = self.pmos_model_choices,
-                nmos_model_choices = self.nmos_model_choices
             )
             self.subcircuit(and_gate)
             self.and_gates.append(and_gate)
@@ -212,19 +74,16 @@ class DECODER3_8(BaseSubcircuit):  # 38译码器+使能EN端
         self.and_for_en = []
         for _ in range(8):
             and_for_en = AND2(
-                nmos_model_name=nmos_model_name,
-                pmos_model_name=pmos_model_name,
-                base_nand_pmos_width=base_nand_pmos_width,
-                base_nand_nmos_width=base_nand_nmos_width,
-                base_inv_pmos_width=base_inv_pmos_width,
-                base_inv_nmos_width=base_inv_nmos_width,
+                nmos_model_nand=nmos_model_nand,
+                pmos_model_nand=pmos_model_nand,
+                nmos_model_inv=nmos_model_inv,
+                pmos_model_inv=pmos_model_inv,
+                nand_pmos_width=nand_pmos_width,
+                nand_nmos_width=nand_nmos_width,
+                inv_pmos_width=inv_pmos_width,
+                inv_nmos_width=inv_nmos_width,
                 length=length,
                 w_rc=w_rc,
-                pi_res=pi_res,
-                pi_cap=pi_cap,
-                sweep_decoder=self.sweep_decoder,
-                pmos_model_choices = self.pmos_model_choices,
-                nmos_model_choices = self.nmos_model_choices
             )
             self.subcircuit(and_for_en)
             self.and_for_en.append(and_for_en)
@@ -271,19 +130,15 @@ class DECODER_CASCADE(BaseSubcircuit):
     """级联行地址译码器，支持任意行数"""
     NAME = "DECODER_CASCADE"
     
-    def __init__(self, nmos_model_name, pmos_model_name, num_rows=16,
-                 base_inv_pmos_width=0.27e-6, base_inv_nmos_width=0.09e-6,
-                 base_nand_pmos_width=0.27e-6, base_nand_nmos_width=0.18e-6,
+    def __init__(self, nmos_model_inv, pmos_model_inv, nmos_model_nand, pmos_model_nand, num_rows=16,
+                 inv_pmos_width=0.27e-6, inv_nmos_width=0.09e-6,
+                 nand_pmos_width=0.27e-6, nand_nmos_width=0.18e-6,
                  length=0.05e-6,
-                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF,sweep_decoder=False,
-                pmos_model_choices = 'PMOS_VTG',nmos_model_choices = 'NMOS_VTG'
+                 w_rc=False, pi_res=100 @ u_Ohm, pi_cap=0.001 @ u_pF,
                  ):
         # 计算地址位数和级数
         self.w_rc=w_rc
         self.num_rows = num_rows
-        self.sweep_decoder = sweep_decoder
-        self.pmos_model_choices=pmos_model_choices
-        self.nmos_model_choices=nmos_model_choices
         self.n_bits = ceil(log2(num_rows)) if num_rows > 1 else 1
         self.n_levels = ceil(self.n_bits / 3.0)
         #NODES = ['VDD', 'VSS','EN'] + [f'A{i}'for i in range(self.n_bits)] + [f'WL{i}' for i in range(num_rows)]
@@ -301,17 +156,14 @@ class DECODER_CASCADE(BaseSubcircuit):
         nodes.extend([f'WL{i}' for i in range(num_rows)])
         self.NODES = nodes
         super().__init__(
-            nmos_model_name, pmos_model_name,
-            base_nand_nmos_width, base_nand_pmos_width, length,
+            nmos_model_inv, pmos_model_inv,
+            nand_nmos_width, nand_pmos_width, length,
             w_rc=w_rc, pi_res=pi_res, pi_cap=pi_cap,
         )
-        
-        
 
         # 存储每级译码器和输出节点
         self.decoders_by_level = []
         self.level_output_nodes = [[] for _ in range(self.n_levels)]
-
 
         # 创建所有译码器
         for level in range(self.n_levels):
@@ -329,17 +181,16 @@ class DECODER_CASCADE(BaseSubcircuit):
                         address_nodes.append('VSS')  # 地址不足时接地
                 # 创建译码器实例
                 decoder = DECODER3_8(
-                    nmos_model_name=nmos_model_name,
-                    pmos_model_name=pmos_model_name,
-                    base_inv_pmos_width=base_inv_pmos_width,
-                    base_inv_nmos_width=base_inv_nmos_width,
-                    base_nand_pmos_width=base_nand_pmos_width,
-                    base_nand_nmos_width=base_nand_nmos_width,
+                    nmos_model_inv=nmos_model_inv,
+                    pmos_model_inv=pmos_model_inv,
+                    nmos_model_nand=nmos_model_nand,
+                    pmos_model_nand=pmos_model_nand,
+                    inv_pmos_width=inv_pmos_width,
+                    inv_nmos_width=inv_nmos_width,
+                    nand_pmos_width=nand_pmos_width,
+                    nand_nmos_width=nand_nmos_width,
                     length=length,
                     w_rc=self.w_rc,
-                    sweep_decoder=self.sweep_decoder,
-                    pmos_model_choices = self.pmos_model_choices,
-                    nmos_model_choices = self.nmos_model_choices
                 )
                 self.subcircuit(decoder)
                 level_decoders.append(decoder)
@@ -378,33 +229,3 @@ class DECODER_CASCADE(BaseSubcircuit):
                        *output_nodes
                        )
             self.decoders_by_level.append(level_decoders)
-
-
-
-if __name__ == '__main__':
-    # 创建3-8译码器实例
-    decoder = DECODER_CASCADE(
-        nmos_model_name="NMOS_VTG",
-        pmos_model_name="PMOS_VTG",
-        num_rows=2,
-        base_inv_pmos_width=0.27e-6,
-        base_inv_nmos_width=0.09e-6,
-        base_nand_pmos_width=0.27e-6,
-        base_nand_nmos_width=0.18e-6,
-        length=0.05e-6,
-        w_rc=False
-    )
-
-    # 打印网表信息
-    print("=" * 80)
-    print("CASCADE DECODER NETLIST")
-    print("=" * 80)
-    print(decoder)
-    print("=" * 80)
-
-    # 打印结构信息
-    print("\nDecoder Structure:")
-    print(f"- Total levels: {decoder.n_levels}")
-    for level, count in enumerate(decoder.level_groups):
-        print(f"  Level {level}: {count} decoders")
-    print(f"Total word lines: {decoder.num_rows}")
